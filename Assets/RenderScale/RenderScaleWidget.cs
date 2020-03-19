4751b6872bd4ef7b770c958de3bd7dd43fdd9c07
jnhtt <man.in.cyber.space@gmail.com>
2020-03-19 23:54:30 +0900

    first strike

---
+++ Assets/RenderScale/RenderScaleWidget.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
+
+public class RenderScaleWidget : MonoBehaviour
+{
+    [SerializeField]
+    private Slider renderScaleSlider;
+
+    private UniversalRenderPipelineAsset pipelineAsset;
+
+    private void Start()
+    {
+        pipelineAsset = GraphicsSettings.currentRenderPipeline as UniversalRenderPipelineAsset;
+        renderScaleSlider.onValueChanged.AddListener(OnValueChange);
+
+        OnValueChange(pipelineAsset.renderScale);
+    }
+
+    private void OnValueChange(float value)
+    {
+        if (pipelineAsset != null)
+        {
+            pipelineAsset.renderScale = value;
+        }
+    }
+}
